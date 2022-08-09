10c859d89584e11f70bbd3d9a5bf7ea329a64d0b
Mariaslavovaa <mimi161514@gmail.com>
2022-08-09 18:13:41 +0300

    added some users to database upon start up

---
+++ management.system/src/main/resources/data.sql
@@ -0,0 +1,31 @@
+truncate table users;
+
+insert into users(username, password, email, full_name)
+VALUES ('maria', '1234567', 'maria@prosta.mnogo', 'Maria Slavova');
+
+insert into users(username, password, email, full_name)
+VALUES ('dimitar', '9999999999', 'dsotirov@gmail.com', 'Dimitar Sotirov');
+
+insert into users(username, password, email, full_name)
+VALUES ('petya', '9484028456', 'pepi@abv.bg', 'Petya Stoyanova');
+
+insert into users(username, password, email, full_name)
+VALUES ('kali', '00000', 'k.ivanova@abv.bg', 'Kalina Kaloyanova');
+
+insert into users(username, password, email, full_name)
+VALUES ('kalina', '000550', 'k.i@abv.bg', 'Kalina Ivaylova');
+
+insert into users(username, password, email, full_name)
+VALUES ('lili', '258963', 'lili99@abv.bg', 'Lili Danailova');
+
+insert into users(username, password, email, full_name)
+VALUES ('peshko', '01254789635', 'petar.ch@gmail.com', 'Petar Chuklev');
+
+insert into users(username, password, email, full_name)
+VALUES ('viki', '222222a', 'atanasova.v@abv.bg', 'Viktoria Atanasova');
+
+insert into users(username, password, email, full_name)
+VALUES ('kalata', 'a4a4a4a4a4a', 'k.kal@abv.bg', 'Kaloqn Kaloqnov');
+
+insert into users(username, password, email, full_name)
+VALUES ('didi', '74d74d74d', null, 'Dilyana Slavova');
